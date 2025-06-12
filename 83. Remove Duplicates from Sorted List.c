abb900338464781d297094a445024b4c817709d2
siesta0Bot <yangtingjun846@gmail.com>
2025-06-12 13:48:05 +0800

    20250612

---
+++ 83. Remove Duplicates from Sorted List.c
@@ -0,0 +1,19 @@
+struct ListNode {
+    int val;
+    struct ListNode *next;
+};
+
+struct ListNode* deleteDuplicates(struct ListNode* head) {
+    struct ListNode *cur = head;
+    while(cur && cur->next){
+        if(cur->val == cur->next->val){
+            struct ListNode *temp = cur->next;
+            cur->next = cur->next->next;
+            free(temp);
+        }
+        else{
+            cur = cur->next;
+        }
+    }
+    return head;
+}
