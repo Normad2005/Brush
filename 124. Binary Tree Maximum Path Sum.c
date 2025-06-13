52f0512ab49da691e139dbdeb56fae7ce6b4cc0c
siesta0Bot <yangtingjun846@gmail.com>
2025-06-13 15:42:35 +0800

    my first hard

--- 124. Binary Tree Maximum Path Sum.c
+++ 124. Binary Tree Maximum Path Sum.c
@@ -1,2 +1,31 @@
 #include <stdlib.h>
+#include <limits.h>
+struct TreeNode {
+    int val;
+    struct TreeNode *left;
+    struct TreeNode *right;
+};
 
+typedef struct TreeNode TreeNode;
+
+int Max(int a, int b){
+    return (a > b) ? a : b;
+}
+
+int dfs(TreeNode *root, int *maxSum){
+    if(!root)
+        return 0;
+    
+    int left = Max(0, dfs(root->left, maxSum));
+    int right = Max(0, dfs(root->right, maxSum));
+
+    *maxSum = Max(*maxSum, left + right + root->val);
+    
+    return Max(left, right) + root->val;
+}
+
+int maxPathSum(struct TreeNode* root) {
+    int maxSum = INT_MIN;
+    dfs(root, &maxSum);
+    return maxSum;
+}
